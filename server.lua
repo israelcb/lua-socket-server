fdf84e287f3e23945c63fe9efec4d310e265ad1b
israelcb <israel.batista031@gmail.com>
2026-08-24 07:48:25 -0300

    Building a funcional socket server with two threads

--- server.lua
+++ server.lua
@@ -1,10 +1,17 @@
-local lanes = require("lanes").configure()
+function data_loop(linda, server_data)
+    local _, fn = linda:receive(nil, "update_server_data")
 
-function server_init(socket)
+    fn(server_data)
+    linda:set("product_list", server_data.product_list)
+
+    data_loop(linda, server_data)
+end
+
+local function server_init(socket)
     local server = socket.tcp()
     assert(server:bind('*', 3000))
     server:listen(5)
-    
+
     local ip, port = server:getsockname()
     print("Servidor ativo e escutando em")
     print(ip .. ":" .. port)
@@ -12,20 +19,44 @@ function server_init(socket)
     return server
 end
 
-function data_loop()
-    print("Entrou!!")
-end
-
-lanes.gen(data_loop)()
-
-local socket = require("socket")
-local server = server_init(socket)
-local clients = {}
-
-while true do
+function server_loop(linda, server)
+    local socket = require("socket")
+    local cjson = require("cjson")
+    
+    if server == nil then
+        server = server_init(socket)
+        return server_loop(linda, server)
+    end
+    
     local client = server:accept()
     local message = client:receive("*l") or "<empty>"
+    local product = cjson.decode(message)
+    
+    linda:send("update_server_data", function(data)
+        table.insert(data.product_list, product)
+    end)
+    
+    socket.sleep(1)
+    local product_list = linda:get("product_list") or {}
+    
+    print("\n[")
+    for k, v in ipairs(product_list) do
+        print("\t" .. cjson.encode(v))
+    end
+    print("]\n")
 
-    print("Received: " .. message)
-    client:send("response\n")
+    -- print("Received: " .. message)
+    -- client:send("response\n")
+
+    server_loop(linda, server)
 end
+
+local lanes = require("lanes").configure()
+local linda = lanes.linda()
+
+local conf = { globals = _G }
+local server_data = { product_list = {} }
+lanes.gen("*", conf, data_loop)(linda, server_data)
+local a = lanes.gen("*", conf, server_loop)(linda)
+
+print(a[1])
