cc93aa6603170f8a699bb4a8ec5e8e7cd17c06ee
israelcb <israel.batista031@gmail.com>
2026-08-23 23:25:39 -0300

    Adding lanes lib

--- server.lua
+++ server.lua
@@ -1,8 +1,10 @@
+local lanes = require("lanes").configure()
+
 function server_init(socket)
     local server = socket.tcp()
     assert(server:bind('*', 3000))
     server:listen(5)
-
+    
     local ip, port = server:getsockname()
     print("Servidor ativo e escutando em")
     print(ip .. ":" .. port)
@@ -10,13 +12,19 @@ function server_init(socket)
     return server
 end
 
+function data_loop()
+    print("Entrou!!")
+end
+
+lanes.gen(data_loop)()
+
 local socket = require("socket")
 local server = server_init(socket)
 local clients = {}
 
 while true do
     local client = server:accept()
-    local message = client:receive("*l") or "<vazio>"
+    local message = client:receive("*l") or "<empty>"
 
     print("Received: " .. message)
     client:send("response\n")
